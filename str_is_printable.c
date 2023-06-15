42bf3866c296d854e4abaea200815d8d82599e6f
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-15 16:34:10 +0300

    c

---
+++ str_is_printable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int  ft_str_is_printable(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 32 && str[i] <= 126))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int main()
+{
+	char c[] = "sda";
+    char a[] = {5};
+	printf("%d\n", ft_str_is_printable(c));
+    printf("%d", ft_str_is_printable(a));
+}
