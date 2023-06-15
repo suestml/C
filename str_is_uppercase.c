3b6b40b9e37c93dfdf340a50ce199825f2777903
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-15 16:33:07 +0300

    c

---
+++ str_is_uppercase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int ft_str_is_uppercase(char *str)
+{
+ 	int i;
+	
+	i = 0;
+   while (str[i] != '\0')
+   {
+   		if (!(str[i] >= 'A' && str [i] <= 'Z'))
+		{
+		 	return (0);
+		}
+		i++;
+   }
+	return (1);   
+}
+
+int main()
+{
+	char x[] = "AA";
+
+	printf("%d", ft_str_is_uppercase(x));	
+}
