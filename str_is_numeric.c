6bf5aa1f4e538fb337fe4f210d1eb983e6ef8c6b
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-15 16:31:34 +0300

    c

---
+++ str_is_numeric.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 48  && str[i] <= 57))
+		{
+			return(0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int main(void)
+{
+	char a[] = "9";
+	printf("%d", ft_str_is_numeric(a));
+
+}
