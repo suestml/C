4269e089a4a2cda98a98a7ac0c18936424bdd15c
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-15 16:35:54 +0300

    c

---
+++ strlowcase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int main()
+{
+	char a[] = "ADFJDDKAIIId3";
+
+	printf("%s", ft_strlowcase(a));
+}
