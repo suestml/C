9d16be904f1e286f9fa09d5305b97016a6cdbe54
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-15 16:29:31 +0300

    c

---
+++ strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+char    *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int    i;
+
+    i = 0;
+    while (src[i] != '\0' && i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return (dest);
+}
+
+int main()
+{
+    char src[] = "selam";
+    char dest[] = "merhaba";
+    printf("%s", ft_strncpy(dest, src, 5));
+    return (0);
+}
