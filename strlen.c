e6700a8493b15d9cbc705ddaa5398875f5ad8cc2
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-16 16:56:27 +0300

    c

--- strlen.c
+++ strlen.c
@@ -9,3 +9,25 @@ int	ft_strlen(char *str)
 	}
 	return (i);
 }
+
+
+
+
+
+#another code of strlen function
+int    ft_strlen(char *str)
+{
+    int    i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+#include <stdio.h>
+int main()
+{
+    char a[] = "hello";
+    printf("%d", ft_strlen(a));
+}
