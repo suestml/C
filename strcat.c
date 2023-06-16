31546e5dcd3bb68e3f5ee1bd3e32f6dd8bd514ed
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-16 16:47:24 +0300

    c

---
+++ strcat.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h> 
+
+char    *ft_strcat(char *dest, char *src)
+{
+    int    i;
+    int    j;
+
+    i = 0;
+    j = 0;
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int main()
+{
+    char a[] = "hello";
+    char b[] = " selam";
+    printf("%s", ft_strcat(a, b));
+}
