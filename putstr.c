5dffc1a1a295f7b998631f80a4cc8ab8db456e40
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-16 16:58:21 +0300

    c

--- putstr.c
+++ putstr.c
@@ -9,3 +9,31 @@ void	ft_putstr(char *str)
 		i++;
 	}
 }
+
+
+
+
+
+# another code of putstr function
+
+
+#include <unistd.h>
+void    ft_putstr(char    *str)
+{
+    int    i;
+
+    i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+int main()
+{
+    char a[] = "hello, world!";
+  //char *b;
+  //b = a;
+  //ft_putstr(b);
+    ft_putstr(a);
+}
