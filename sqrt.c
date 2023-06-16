0226714a4bfdad01825208e3182d1a0ad33d6238
Süeda <103206238+suestml@users.noreply.github.com>
2023-06-16 17:07:02 +0300

    c

---
+++ sqrt.c
@@ -0,0 +1,30 @@
+int    ft_sqrt(int nb)
+{
+    long    index;
+    long    b;
+
+    b = nb;
+    if (b <= 0)
+        return (0);
+    if (b == 1)
+        return (1);
+    index = 2;
+    if (b >= 2)
+    {
+        while (index * index <= b)
+        {
+            if (index * index == b)
+            {
+                return (index);
+            }
+            index++;
+        }
+    }
+    return (0);
+}
+#include <stdio.h>
+int    main(void)
+{
+	printf("%d", ft_sqrt(2));
+    return (0);
+}
